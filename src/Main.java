90d60fda6964bc3d7266c4511bea5197111713c4
prabin <prabinyadav.p@gorfugal.com>
2023-11-23 20:54:00 +0530

    Removing unwanted codes in main

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("Hello world!");
     }
 }
-=======
 
-            System.out.println("Hello Prabin");
-        }
-
-    }
->>>>>>> main
